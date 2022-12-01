6ac4292a5442ff324ab578a171108433c16ee524
Markus Neteler <neteler@gmail.com>
2022-12-01 16:19:08 +0100

    test.sh: fix LST download; fix test dir

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -3,22 +3,32 @@
 # fail on non-zero return code from a subprocess
 set -e
 
-# download NC test location if the test needs the data and run tests
+#GRASSDB=/grassdb
+GRASSDB=/tmp
+
+# download NC test location if the test needs the data
 if [ "$1" == "NC" ]
 then
+  # this is run in nc_spm_empty
   g.extension g.download.location
-  g.download.location url=https://grass.osgeo.org/sampledata/north_carolina/nc_spm_full_v2alpha2.tar.gz path=/grassdb
-  g.mapset mapset=PERMANENT location=nc_spm_full_v2alpha2 -c
-  g.list all
-fi
+  # fetch default NC location
+  g.download.location url=https://grass.osgeo.org/sampledata/north_carolina/nc_spm_08_grass7.tar.gz path=$GRASSDB
+  g.mapset location=nc_spm_08_grass7 mapset=PERMANENT -c
+  r.info -g elevation
 
-# download NC MODIS LST time series and unpack in nc_spm_full_v2alpha2
-cd /grassdb/nc_spm_full_v2alpha2/ && \
+  # download NC MODIS LST time series and unpack it in nc_spm_08_grass7
+  cd $GRASSDB/nc_spm_08_grass7/ && \
 	wget https://grass.osgeo.org/sampledata/north_carolina/nc_spm_mapset_modis2015_2016_lst.zip && \
 	unzip nc_spm_mapset_modis2015_2016_lst.zip && \
 	rm -f nc_spm_mapset_modis2015_2016_lst.zip
+  g.mapset location=nc_spm_08_grass7 mapset=modis_lst -c
+  r.info -g MOD11B3.A2015182.h11v05.single_LST_Day_6km
+fi
 
-# run all tests in folder
+# run tests in docker space
+cd /src
+
+# collect and run all tests in folder
 FILENAME=$(basename "$(find . -name *.html -maxdepth 1)")
 ADDON="${FILENAME%%.html}"
 
